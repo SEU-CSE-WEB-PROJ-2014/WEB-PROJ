825a4c23a919ecae679a9084f29a7d2449925ef2
JALL <jallenjia@sina.com>
2014-12-26 08:51:27 +0800

    add method to refresh spring config.

--- src/com/easygo/serverManager/web/ServerManagerController.java
+++ src/com/easygo/serverManager/web/ServerManagerController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.alibaba.fastjson.JSON;
@@ -32,9 +33,11 @@ public class ServerManagerController {
 		Byte status = Constant.STATUS_ERR;
 		String msg = null;
 		try{
-//			WebApplicationContext wac
-//			 = WebApplicationContextUtils.getWebApplicationContext(request.getContext());
-			
+			WebApplicationContext wac
+			 = WebApplicationContextUtils.getWebApplicationContext(request.getSession(true).getServletContext());
+			XmlWebApplicationContext context = (XmlWebApplicationContext) wac;
+			context.refresh();
+			status = Constant.STATUS_SUCC;
 		}catch (Exception e) {
 			e.printStackTrace();
 			msg = e.getMessage();
