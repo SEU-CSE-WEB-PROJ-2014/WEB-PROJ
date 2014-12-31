68b9193d4e23f00cf458af3ab6ab971155366aa1
jallenjia <jallenjia@sina.com>
2014-12-31 14:45:45 +0800

    Backstage GoodsType grid.

--- src/com/easygo/goods/web/BSGoodsTypeController.java
+++ src/com/easygo/goods/web/BSGoodsTypeController.java
@@ -1,7 +1,11 @@
 package com.easygo.goods.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller("bsGoodsType")
@@ -12,6 +16,14 @@ public class BSGoodsTypeController {
 		return new ModelAndView("goods/bsGoodsType/index");
 	}
 	
-	
+	public ModelAndView goodsTypeGrid(@RequestParam(required=false) Integer pageSize,
+			@RequestParam(required=false) Integer pageNum){
+		Map result = new HashMap<String, Object>();
+		
+		//TODO: result里放分页的goodsType数据
+		
+		
+		return new ModelAndView("", result);
+	}
 	
 }
