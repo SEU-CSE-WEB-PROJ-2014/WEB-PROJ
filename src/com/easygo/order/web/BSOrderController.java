a0fccab671d8e2f5ec3f0542648b01278b874ab5
jallenjia <jallenjia@sina.com>
2014-12-31 16:59:33 +0800

    add bs order controller.

--- src/com/easygo/order/web/BSOrderController.java
+++ src/com/easygo/order/web/BSOrderController.java
@@ -1,7 +1,11 @@
 package com.easygo.order.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -11,6 +15,36 @@ public class BSOrderController {
 	@RequestMapping("/index.do")
 	public ModelAndView index(){
 		return new ModelAndView("order/bsOrder/index");
+	}
+	
+	
+	public ModelAndView orderGrid(
+			@RequestParam Integer pageSize,
+			@RequestParam Integer pageNum,
+			@RequestParam(required=false) Integer payState,
+			@RequestParam(required=false) Integer transState,
+			@RequestParam(required=false) Integer signState){
+		Map result = new HashMap<String, Object>();
+		
+		//调用service层,返回分页用户数据:
+		
+		return new ModelAndView("", result);
+	}
+	
+	public void setOrderTransState(
+			@RequestParam String orderId,
+			@RequestParam Integer transState){
+		
+		//TODO: 掉用service层方法,设置订单发货状态
+		
+	}
+	
+	
+	public void batchCancelOrder(
+			@RequestParam String[] orders){
+		
+		//TODO: 调用service层方法,删除对应userIds的用户
+		
 	}
 
 }
