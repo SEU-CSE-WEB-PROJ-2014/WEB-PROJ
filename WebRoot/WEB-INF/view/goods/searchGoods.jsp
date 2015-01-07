80e7e8a2f7693825377839d7da329cc999e427c7
jallenjia <jallenjia@sina.com>
2015-01-07 15:11:44 +0800

    goods search for forestage.

--- WebRoot/WEB-INF/view/goods/searchGoods.jsp
+++ WebRoot/WEB-INF/view/goods/searchGoods.jsp
@@ -1,9 +1,14 @@
 <%@ page language="java" import="java.util.*" pageEncoding="utf-8" session="false"%>
 <%@ include file="/WEB-INF/common/header.jsp" %>
 
+<!-- 搜索条件 -->
 <div>
-  EasyGo home page. <br>
 </div>
+
+<!-- 商品列表 -->
+<div>
+</div>
+
 
 <script type="text/javascript">
 $(function(){
---
+++ src/com/easygo/goods/web/GoodsController.java
@@ -0,0 +1,15 @@
+package com.easygo.goods.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller("goods")
+@RequestMapping("/goods")
+public class GoodsController {
+	public ModelAndView goodsSearch(){
+		
+		
+		return new ModelAndView("goods/goodsSearch");
+	}
+}
